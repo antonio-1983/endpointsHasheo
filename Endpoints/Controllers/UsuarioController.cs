e176b55774a00c13e5205dec3615892a7b914859
antonio-1983 <antoniocastro2024@gmail.com>
2023-12-04 09:27:11 -0300

    primer commit

---
+++ Endpoints/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Datos;
+using Endpoints.Modelo;
+
+namespace Endpoints.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController :  ControllerBase
+    {
+        public UsuarioController()
+        {
+
+        }
+
+        [HttpGet("usuario/{id}")]
+        public Usuario usuarioGet(int id)
+        {
+            Dato usuarioDato = new Dato();
+            Usuario modelo = new Usuario();
+            modelo = usuarioDato.ObtenerUsuario(id);
+            return modelo;
+        }
+
+        [HttpPost("usuario")]
+        public Usuario usuarioPost(Usuario modelo)
+        {
+            Dato usuarioDato = new Dato();
+            usuarioDato.InsertarUsuario(modelo.Nombre, modelo.Email, modelo.Contrasena);
+            return modelo;
+        }
+
+        [HttpPatch("ActualizarUsuario/{id}")]
+        public Usuario usuarioPut(int id)
+        {
+            Dato usuarioDato = new Dato();
+            Usuario modelo = new Usuario();
+            modelo = usuarioDato.ObtenerUsuario(id);
+            usuarioDato.ActualizarUsuario(modelo.Id, modelo.Nombre, modelo.Email, modelo.Contrasena);
+            return modelo;
+        }
+
+        [HttpDelete("EliminarUsuario/{id}")]
+        public string usuarioDelete(int id)
+        {
+            Dato usuarioDato = new Dato();
+            usuarioDato.EliminarUsuario(id);
+            return "usuario eliminado";
+        }
+
+        [HttpPost("login")]
+        public void usuarioLogin(Usuario modelo)
+        {
+            Dato usuarioDato = new Dato();
+            usuarioDato.LoginUsuario(modelo.Id, modelo.Nombre, modelo.Email, modelo.Contrasena);
+
+        }
+    }
+}
+
---
+++ Endpoints/Modelo/UsuarioModelo.cs
@@ -0,0 +1,24 @@
+﻿namespace Endpoints.Modelo
+{
+    public class UsuarioModelo
+    {
+        public UsuarioModelo() { }
+
+        public UsuarioModelo(int id, string nombre, string email, string contrasena, bool activo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Email = email;
+            Contrasena = contrasena;
+            Activo = activo;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contrasena { get; set; }
+        public bool Activo { get; set; }
+    }
+}
